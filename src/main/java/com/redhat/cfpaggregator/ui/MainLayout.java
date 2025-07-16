2b112a7dda23216462b0a82c364f73c23f0ea4ad
Eric Deandrea <eric.deandrea@gmail.com>
2025-07-16 09:10:00 -0400

    Add theme toggle

--- src/main/java/com/redhat/cfpaggregator/ui/MainLayout.java
+++ src/main/java/com/redhat/cfpaggregator/ui/MainLayout.java
@@ -1,33 +1,69 @@
 package com.redhat.cfpaggregator.ui;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
-import com.redhat.cfpaggregator.service.CfpService;
 import com.redhat.cfpaggregator.ui.views.ManagePortalsView;
 import com.redhat.cfpaggregator.ui.views.SearchEventsView;
 
 @PageTitle("CFP Aggregator")
 @Route("")
+@JsModule("prefers-color-scheme.js")
 public class MainLayout extends AppLayout {
-  private final CfpService cfpService;
+  private final Button switchThemeButton = new Button(VaadinIcon.SUN_O.create());
+  private boolean isDefaultDarkTheme = false;
+  private String currentTheme = Lumo.LIGHT;
 
-  public MainLayout(CfpService cfpService) {
-    this.cfpService = cfpService;
-    addToNavbar(new DrawerToggle(), new H1("CFP Aggregator"));
+  public MainLayout() {
+    super();
+
+    // Check the browser preference at set theme accordingly
+    UI.getCurrent().getElement().executeJs("return document.documentElement.getAttribute('theme') == 'dark'")
+        .then(
+            Boolean.class,
+            isDark -> {
+              this.isDefaultDarkTheme = isDark;
+              this.currentTheme = isDark ? Lumo.DARK : Lumo.LIGHT;
+
+              this.switchThemeButton.setIcon(this.isDefaultDarkTheme ? VaadinIcon.SUN_O.create() : VaadinIcon.MOON_O.create());
+              this.switchThemeButton.addClickListener(e -> toggleLightDarkTheme());
+            }
+        );
+
+    var titleLayout = new HorizontalLayout(this.switchThemeButton, new H1("CFP Aggregator"));
+    titleLayout.setSpacing(true);
+    addToNavbar(new DrawerToggle(), titleLayout);
 
     var scroller = new Scroller(createNav());
     scroller.setClassName(Padding.SMALL);
     addToDrawer(scroller);
     setPrimarySection(Section.DRAWER);
+  }
+
+  private void toggleLightDarkTheme() {
+    if (Lumo.DARK.equals(this.currentTheme)) {
+      this.currentTheme = Lumo.LIGHT;
+      this.switchThemeButton.setIcon(VaadinIcon.MOON_O.create());
+    }
+    else {
+      this.currentTheme = Lumo.DARK;
+      this.switchThemeButton.setIcon(VaadinIcon.SUN_O.create());
+    }
+
+    UI.getCurrent().getElement().executeJs("document.documentElement.setAttribute('theme', '" + this.currentTheme + "')");
   }
 
   private SideNav createNav() {
